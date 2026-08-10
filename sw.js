82893713d21d22ec854bc5c4173f4e7f8dbf5af6
Zongyi104 <125478314+Zongyi104@users.noreply.github.com>
2026-08-10 16:49:09 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'attendance-app-v3';
+const CACHE_NAME = 'attendance-app-v4';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
